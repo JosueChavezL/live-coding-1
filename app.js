3c83c41721d83a2f29f466b2171cdc1eb410c5db
Josue Chavez <josue.chavezlomeli@gmail.com>
2022-09-29 14:47:41 -0500

    Primer ejercicio de live coding

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// Escribe tu código aquí.
+let arregloOriginal = ["insecto", "bootcamp", "mangos", "reptil", "mosca", "escritorio", "ae"];
+let arregloModif = [];
+let strPrueba = "ave";
+let lista = document.getElementById("listaMod");
+
+window.addEventListener("load", function (e){
+    arregloModif = compareString(strPrueba,arregloOriginal);
+    console.log(arregloModif);
+
+    arregloModif.forEach(e => {
+        lista.innerHTML += `<li>${e}</li></br>`
+        
+    });
+
+});
+
+
+function compareString(strPrueba, arregloOriginal){
+    let longPalabra = strPrueba.length;
+    let x = [];   
+
+    arregloOriginal.forEach(e => {
+
+        if(e.length > longPalabra){
+            x.push(e);
+        }        
+    }); 
+    return x;
+}//funcion live coding
